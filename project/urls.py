cde17693b672276c5298667b7f9f570e64d93586
watollop <jordillopez@gmail.com>
2016-10-25 01:49:39 +0200

    Tut 25

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^post/', include("posts.url", namespace='posts')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
